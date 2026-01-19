093c5085e71d7ec10dbf1a5a7e4f079c05acc96a
Mike <binarybardcode@gmail.com>
2026-01-19 18:25:19 +0200

    Fix import statement casing for Cocktails component and clean up whitespace in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from "gsap/all"; 
+import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Cocktails from './components/cocktails';
+import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
 import Menu from './components/Menu';
 import Contact from './components/Contact';
 
-gsap.registerPlugin(ScrollTrigger, SplitText); 
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
